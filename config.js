d4494ea03f2cc2e62de0233ae27afa05f385e172
Bhairesh M <bhairesh@arithaconsulting.com>
2021-07-22 20:43:40 +0530

    changes

--- config.js
+++ config.js
@@ -2,6 +2,10 @@ const dotenv = require("dotenv").config();
 
 const env = {
   port: process.env.DD_PORT,
+
+  //Google Drive
+  clientId: process.env.DD_ClientID,
+  clientSecret: process.env.DD_ClientSecret,
 };
 
 exports.env = env;
